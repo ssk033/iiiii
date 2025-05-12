3bce3fc9cf1f8c2dbad7e91edbf2c841324789c5
sanidhya033 <mypersonaldesk1202@gmail.com>
2025-05-13 00:01:21 +0530

    6th commit

--- public/toposort.java
+++ public/toposort.java
@@ -1,54 +1,52 @@
-    import java.util.*;
+import java.util.*;
+
 public class toposort {
-        public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of vertices: ");
         int V = sc.nextInt();
         System.out.print("Enter the number of edges: ");
         int E = sc.nextInt();
 
-        List<List<Integer>> adj=new ArrayList<>();
-        int[] inDegree=new int[V];
-
-        for(int i=0;i<V;i++){
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
             adj.add(new ArrayList<>());
         }
-        
+
         System.out.println("Enter the edges of the graph:");
-        for(int i=0;i<E;i++){
-            int u=sc.nextInt();
-            int v=sc.nextInt();
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt(); 
+            int v = sc.nextInt(); 
             adj.get(u).add(v);
-            inDegree[v]++;
         }
-        Queue<Integer> queue=new LinkedList<>();
-        for(int i=0;i<V;i++){
-            if(inDegree[i]==0){
-                queue.offer(i);
+
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, stack);
             }
         }
 
-        List<Integer> TopoOrder=new ArrayList<>();
-        while(!queue.isEmpty()){
-            int node=queue.poll();
-            TopoOrder.add(node);
+        System.out.println("Topological Sort:");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
 
-            for(int neighbour:adj.get(node)){
-                inDegree[neighbour]--;
-                if(inDegree[neighbour]==0){
-                    queue.offer(neighbour);
-                }
-            }
-        }
-        if (TopoOrder.size() != V) {
-            System.out.println("The graph has a cycle. Topological sort not possible.");
-        } else {
-            System.out.println("Topological Sort:");
-            for (int node : TopoOrder) {
-                System.out.print(node + " ");
-            }
-        }
         sc.close();
-    }    
+    }
+
+    static void dfs(int node, List<List<Integer>> adj, boolean[] visited, Stack<Integer> stack) {
+        visited[node] = true;
+
+        for (int neighbour : adj.get(node)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, adj, visited, stack);
+            }
+        }
+
+        stack.push(node); 
+    }
 }
