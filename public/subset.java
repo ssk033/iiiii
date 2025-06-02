d31728b315afea39e1668a9947175a3f68a642e1
sanidhya033 <mypersonaldesk1202@gmail.com>
2025-06-03 02:58:41 +0530

    15th

--- public/subset.java
+++ public/subset.java
@@ -1,15 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class subset {
 
-    static boolean isSubsetSum(int[] set, int n, int sum) {
+    static boolean isSubsetSum(int[] set, int n, int sum, List<Integer> subset) {
         if (sum == 0)
             return true;
         if (n == 0)
             return false;
+
         if (set[n - 1] > sum)
-            return isSubsetSum(set, n - 1, sum);
-        return isSubsetSum(set, n - 1, sum) || isSubsetSum(set, n - 1, sum - set[n - 1]);
+            return isSubsetSum(set, n - 1, sum, subset);
+
+        if (isSubsetSum(set, n - 1, sum, subset))
+            return true;
+
+        subset.add(set[n - 1]);
+        if (isSubsetSum(set, n - 1, sum - set[n - 1], subset))
+            return true;
+
+        subset.remove(subset.size() - 1);
+        return false;
     }
 
     public static void main(String[] args) {
@@ -28,10 +38,14 @@ public class subset {
         System.out.print("Enter the target sum to find: ");
         int sum = sc.nextInt();
 
-        if (isSubsetSum(set, n, sum)) {
-            System.out.println("\n Found a subset with the given sum.");
+        List<Integer> subset = new ArrayList<>();
+
+        if (isSubsetSum(set, n, sum, subset)) {
+            System.out.println("\nFound a subset with the given sum:");
+            for (int num : subset)
+                System.out.print(num + " ");
         } else {
-            System.out.println("\n No subset with the given sum was found.");
+            System.out.println("\nNo subset with the given sum was found.");
         }
 
         sc.close();
